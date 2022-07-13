b83bd4549bb9c09a04fdb4896352dac18754d78e
root <harshalmahapure44@gmail.com>
2022-07-13 08:10:44 +0000

    cleaned code

--- server/routes/insertUser.js
+++ server/routes/insertUser.js
@@ -11,8 +11,8 @@ async function createUser(body){
     return res;
 }
 
-function createTable(tableName,columns,userdb){
-      userdb.query(
+async function createTable(tableName,columns,userdb){
+    await  userdb.query(
          `CREATE TABLE
           ${tableName}(
              ${columns[0]},
@@ -23,8 +23,7 @@ function createTable(tableName,columns,userdb){
 
 
 async function addUserCredential(body){
-   return new Promise(async(resolve,reject) =>{
-    try{
+  
      await database.query(
            `INSERT INTO
             users(name,user_name,email,password)
@@ -34,12 +33,7 @@ async function addUserCredential(body){
             body.email,
             body.password] 
      );
-    }
-    catch(error){
-        return reject(error);
-    }
-    return resolve();
-   });
+  
 };
 
 
@@ -57,25 +51,19 @@ async function createDatabase(name){
 
 
 async function signUp(body){
-   
-    await  addUserCredential(body).then(async()=>{
-      console.log("hit");
-      await createUser(body);
-      await createDatabase(body.userName);;
-   
-      const userdb = getUserdb(body);
-      const columns = ["todo VARCHAR(100) NOT NULL",
+   try{
+    await  addUserCredential(body);
+    console.log("hit");
+    await createUser(body);
+    await createDatabase(body.userName);;
+    const userdb = getUserdb(body);
+    const columns = ["todo VARCHAR(100) NOT NULL",
         "date DATE NOT NULL"];
-  
-     await createTable("todos",columns,userdb);
-  
-    }).catch(error =>{
+    await createTable("todos",columns,userdb);
+    }
+    catch(error){
         console.log(error.constraint);
-        return;
-    });
-
-   
- 
+    };
 }
 
 
