d5555fc717e66ddf7430c37ce788360b1bf874ba
harshalmahapure44@gmail.com <harshalmahapure44@gmail.com>
2022-07-16 13:40:26 +0530

    added h1 in sign in.html

--- src/index.jsx
+++ src/index.jsx
@@ -3,24 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-   if (parts.length === 2) return parts.pop().split(';').shift();
-}
 
-let token = getCookie("jwt");
-const reqInfo ={
-    method:"POST",
-    headers:{
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-    },
-    body:JSON.stringify({
-        todo:"some todo",
-        date:"2022-07-15 13:15"
-    })
-}
 
 
 ReactDOM.render(
